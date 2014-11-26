fc39af321aebe069fedc1830f3a74385aabd6516
Payam Shoghi <payam49er@gmail.com>
2014-11-25 23:43:24 -0500

    First Non Repeated Character https://www.codeeval.com/open_challenges/12/

---
+++ FirstNonRepeatedChar/FirstNonRepeatedChar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FirstNonRepeatedChar
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineValue = reader.ReadLine();
+
+                    var grouped = lineValue.GroupBy(x => x);
+
+                    foreach (var group in grouped)
+                    {
+                        if (group.Count() == 1)
+                        {
+                            Console.WriteLine(group.Key);
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
