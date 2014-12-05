0d7a7b5e5f6fa472794b17dd419c3c6e9be9eda9
Payam Shoghi <payam49er@gmail.com>
2014-12-05 13:40:50 -0500

    Without Repetitions https://www.codeeval.com/open_challenges/173/

---
+++ DeleteRepetition/DeleteRepetition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace DeleteRepetition
+{
+    using System.IO;
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] lineValue = reader.ReadLine().Split(' ');
+
+                    foreach (string s in lineValue)
+                    {
+                        if (s.Any())
+                        {
+                            Console.Write(s[0]);
+
+                            for (int i = 1; i < s.Count(); i++)
+                            {
+                                if (s[i] != s[i - 1])
+                                {
+                                    Console.Write(s[i]);
+
+                                }
+                            }
+                            Console.Write(' ');
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
